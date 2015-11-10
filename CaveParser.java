1332297945aa94ba4442b9a0a7e77160aa2d7aa7
unknown <nicholas.karsten@cuw.edu>
2015-11-10 01:44:31 -0600

    added rough parse code

--- CaveParser.java
+++ CaveParser.java
@@ -10,6 +10,37 @@ public class CaveParser
 	
 	public JSONObject parse()
 	{
+		for(int i = 0; i < this.theJSON.length(); i++)
+		{
+			if(theJSON.indexOf('{') != -1)
+			{
+				System.out.println("JSONObject begin");
+			}
+			
+		}
 		
+		for(int i = 0; i < this.theJSON.length(); i++)
+		{
+			if(theJSON.indexOf('[') != -1)
+			{
+				System.out.println("JSONArray begin");
+			}
+		}
+		
+		for(int i =0; i < this.theJSON.length(); i++)
+		{
+			if(theJSON.indexOf('}') != -1)
+			{
+				System.out.println("JSONObject end");
+			}
+		}
+		
+		for(int i = 0; i < this.theJSON.length(); i++)
+		{
+			if(theJSON.indexOf(']') != -1)
+			{
+				System.out.println("JSONArray end");
+			}
+		}
 	}
 }
