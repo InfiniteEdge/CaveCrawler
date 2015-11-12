5a88ce6cb0e94488f84ad8c166d99f7c0a07222d
unknown <nicholas.karsten@cuw.edu>
2015-11-12 14:08:09 -0600

    Updated code to where we are in class

---
+++ JSONObjectVariable.java
@@ -0,0 +1,10 @@
+public class JSONObjectVariable extends JSONVariable
+{
+	private JSONObject value;
+	
+	public JSONObjectVariable(String name, JSONObject value)
+	{
+		super(name);
+		this.value = value;
+	}
+}
